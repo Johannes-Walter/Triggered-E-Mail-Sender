1e0317afb108e52cd7049ffdd0a2eea142cf5740
Johannes-Walter <jo.walter97@gmx.de>
2020-12-11 14:21:57 +0100

    Update button_counter.py

--- button_counter.py
+++ button_counter.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
-GPIO.setwarnings(false)
-GPIO.setmode(GPIO(BOARD))
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+print("programm starting")
 while True:
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed")
+        while GPIO.input(10) == GPIO.HIGH:
+            pass
+        print("Button was realeased")
