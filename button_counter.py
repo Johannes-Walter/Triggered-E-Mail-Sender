43f87e76dca658826e78fc364832278a6923af0e
Johannes-Walter <jo.walter97@gmx.de>
2020-12-14 17:07:48 +0100

    Switched from polling to "event-based" buttonreading

--- button_counter.py
+++ button_counter.py
@@ -1,33 +1,41 @@
-from typing import get_args
 from mailsender import send_mail
 import RPi.GPIO as GPIO
 import datetime
 import databasemanager
 import mailsender
+import settings_reader
+import time
 
+def run_loop() -> None:
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    press_start: datetime.datetime
+    press_end: datetime.datetime
+    pin_to_read: int = settings_reader.get_pin_to_read()
+    batch_wait_time: int = settings_reader.get_batch_send_wait_time()
 
-press_start: datetime.datetime
-press_end: datetime.datetime
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin_to_read, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-
-def GPIO_loop():
     print("GPIO_loop starting...")
     while True:
-        if GPIO.input(10) == GPIO.HIGH:
-            press_start = datetime.datetime.now()
-            while GPIO.input(10) == GPIO.HIGH:
-                pass
-            press_end = datetime.datetime.now()
+        # Auf das Betätigen des Knopfes warten
+        GPIO.wait_for_edge(pin_to_read, GPIO.RISING)
+        press_start = datetime.datetime.now()
+        # Knopf entprellen
+        time.sleep(0.05)
 
-            databasemanager.add_button_press(press_start, press_end-press_start)
-            send_button_press(press_start, press_end-press_start)
+        # Auf das Loslassen des Knopfes Warten
+        GPIO.wait_for_edge(pin_to_read, GPIO.FALLING)
+        press_end = datetime.datetime.now()
+        # Knopf entprellen
+        time.sleep(0.05)
+
+        databasemanager.add_button_press(press_start, press_end-press_start)
+        send_button_press(press_start, press_end-press_start)
 
 
-def send_button_press(press_start: datetime.datetime, press_duration: datetime.timedelta):
+def send_button_press(press_start: datetime.datetime, press_duration: datetime.timedelta) -> None:
 
     # Formatiert das Datum nach der im mailsender gegebenen Formatierung
     date = press_start.strftime(mailsender.DATE_FORMAT)
@@ -40,3 +48,6 @@ def send_button_press(press_start: datetime.datetime, press_duration: datetime.t
 
     mailsender.send_mail(subject, message)
 
+
+if __name__ == "__main__":
+    run_loop()
